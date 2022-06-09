0d77105fa73c6eca78d86a68a7df069a62623942
aleramirezsj <aleramirezsj@gmail.com>
2022-06-09 13:52:22 -0300

    Agregar archivos de proyecto.

---
+++ AppPruebasEnBlanco/AppPruebasEnBlanco/Views/Inicio.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPruebasEnBlanco.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void AbrirPruebaDeGrid(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PruebaDeGrid());
+        }
+    }
+}
---
+++ AppPruebasEnBlanco/AppPruebasEnBlanco/Views/PruebaDeGrid.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPruebasEnBlanco.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PruebaDeGrid : ContentPage
+    {
+        public PruebaDeGrid()
+        {
+            InitializeComponent();
+        }
+        private async void CerrarPruebaDeGrid(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+    }
+}
